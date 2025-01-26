a41a4ba1fca3c2ff73103a861a56cb7c2c1c839e
kiz___ <140178579+kizYTB@users.noreply.github.com>
2025-01-26 10:33:55 +0100

    Update speaker.lua

--- sys/apps/speaker.lua
+++ sys/apps/speaker.lua
@@ -1,178 +1,117 @@
--- SPDX-FileCopyrightText: 2021 The CC: Tweaked Developers
---
--- SPDX-License-Identifier: MPL-2.0
+-- Charger la bibliothèque DFPWM
+local dfpwm = require("NewLua.Audio.dfpwm")
 
-local function get_speakers(name)
-    if name then
-        local speaker = peripheral.wrap(name)
-        if speaker == nil then
-            error(("Speaker %q does not exist"):format(name), 0)
-            return
-        elseif not peripheral.hasType(name, "speaker") then
-            error(("%q is not a speaker"):format(name), 0)
-        end
-
-        return { speaker }
-    else
-        local speakers = { peripheral.find("speaker") }
-        if #speakers == 0 then
-            error("No speakers attached", 0)
-        end
-        return speakers
+-- Téléchargement d'un fichier depuis une URL
+local function downloadFile(url, outputFile)
+    print("Téléchargement du fichier depuis l'URL :", url)
+    local response = http.get(url)
+    if not response then
+        error("Échec du téléchargement depuis l'URL : " .. url)
     end
+
+    local file = fs.open(outputFile, "wb")
+    file.write(response.readAll())
+    file.close()
+    response.close()
+
+    print("Fichier téléchargé et sauvegardé sous :", outputFile)
 end
 
-local function pcm_decoder(chunk)
-    local buffer = {}
-    for i = 1, #chunk do
-        buffer[i] = chunk:byte(i) - 128
+-- Lecture de fichiers DFPWM
+local function playDFPWM(filePath)
+    local decoder = dfpwm.make_decoder()
+    local speaker = peripheral.find("speaker")
+
+    if not speaker then
+        error("Aucun périphérique speaker détecté.")
     end
-    return buffer
-end
 
-local function report_invalid_format(format)
-    printError(("speaker cannot play %s files."):format(format))
-    local pp = require "cc.pretty"
-    pp.print("Run '" .. pp.text("help speaker", colours.lightGrey) .. "' for information on supported formats.")
-end
-
-
-local cmd = ...
-if cmd == "stop" then
-    local _, name = ...
-    for _, speaker in pairs(get_speakers(name)) do speaker.stop() end
-elseif cmd == "play" then
-    local _, file, name = ...
+    local file = fs.open(filePath, "rb")
     if not file then
-        error("Usage: speaker play <file or url> [speaker]", 0)
+        error("Impossible d'ouvrir le fichier DFPWM : " .. filePath)
     end
 
-    local speaker = get_speakers(name)[1]
+    print("Lecture du fichier DFPWM :", filePath)
 
-    local handle, err
-    if http and file:match("^https?://") then
-        print("Downloading...")
-        handle, err = http.get(file)
-    else
-        handle, err = fs.open(shell.resolve(file), "r")
-    end
-
-    if not handle then
-        printError("Could not play audio:")
-        error(err, 0)
-    end
-
-    local start = handle.read(4)
-    local pcm = false
-    local size = 16 * 1024 - 4
-    if start == "RIFF" then
-        handle.read(4)
-        if handle.read(8) ~= "WAVEfmt " then
-            handle.close()
-            error("Could not play audio: Unsupported WAV file", 0)
-        end
-
-        local fmtsize = ("<I4"):unpack(handle.read(4))
-        local fmt = handle.read(fmtsize)
-        local format, channels, rate, _, _, bits = ("<I2I2I4I4I2I2"):unpack(fmt)
-        if not ((format == 1 and bits == 8) or (format == 0xFFFE and bits == 1)) then
-            handle.close()
-            error("Could not play audio: Unsupported WAV file", 0)
-        end
-        if channels ~= 1 or rate ~= 48000 then
-            print("Warning: Only 48 kHz mono WAV files are supported. This file may not play correctly.")
-        end
-        if format == 0xFFFE then
-            local guid = fmt:sub(25)
-            if guid ~= "\x3A\xC1\xFA\x38\x81\x1D\x43\x61\xA4\x0D\xCE\x53\xCA\x60\x7C\xD1" then -- DFPWM format GUID
-                handle.close()
-                error("Could not play audio: Unsupported WAV file", 0)
-            end
-            size = size + 4
-        else
-            pcm = true
-            size = 16 * 1024 * 8
-        end
-
-        repeat
-            local chunk = handle.read(4)
-            if chunk == nil then
-                handle.close()
-                error("Could not play audio: Invalid WAV file", 0)
-            elseif chunk ~= "data" then -- Ignore extra chunks
-                local size = ("<I4"):unpack(handle.read(4))
-                handle.read(size)
-            end
-        until chunk == "data"
-
-        handle.read(4)
-        start = nil
-    -- Detect several other common audio files.
-    elseif start == "OggS" then return report_invalid_format("Ogg")
-    elseif start == "fLaC" then return report_invalid_format("FLAC")
-    elseif start:sub(1, 3) == "ID3" then return report_invalid_format("MP3")
-    elseif start == "<!DO" --[[<!DOCTYPE]] then return report_invalid_format("HTML")
-    end
-
-    print("Playing " .. file)
-
-    local decoder = pcm and pcm_decoder or require "NewLua.Audio.dfpwm".make_decoder()
     while true do
-        local chunk = handle.read(size)
+        local chunk = file.read(16 * 1024) -- Lire par blocs de 16 Ko
         if not chunk then break end
-        if start then
-            chunk, start = start .. chunk, nil
-            size = size + 4
-        end
 
-        local buffer = decoder(chunk)
-        while not speaker.playAudio(buffer) do
-            os.pullEvent("speaker_audio_empty")
-        end
+        local decoded = decoder(chunk)
+        speaker.playAudio(decoded)
     end
 
-    handle.close()
-elseif cmd == "sound" then
-    local _, sound, volume, pitch, name = ...
+    file.close()
+    print("Lecture terminée :", filePath)
+end
 
-    if not sound then
-        error("Usage: speaker sound <sound> [volume] [pitch] [speaker]", 0)
-        return
+-- Lecture d'un fichier DFPWM à partir d'une URL
+local function playDFPWMFromURL(url)
+    if not http then
+        error("L'API HTTP n'est pas activée.")
     end
 
-    if volume then
-        volume = tonumber(volume)
-        if not volume then
-            error("Volume must be a number", 0)
-        end
-        if volume < 0 or volume > 3 then
-            error("Volume must be between 0 and 3", 0)
-        end
+    local tempFile = "/tmp/temp.dfpwm" -- Fichier temporaire pour sauvegarder l'audio
+    downloadFile(url, tempFile)
+    playDFPWM(tempFile)
+    fs.delete(tempFile) -- Nettoyer le fichier temporaire après la lecture
+end
+
+-- Afficher l'aide de la commande
+local function showHelp()
+    print("Utilisation : speaker <commande> [arguments]")
+    print("Commandes disponibles :")
+    print("  playSound <nom> [volume] [pitch]  - Joue un son Minecraft.")
+    print("  playDFPWM <fichier>               - Joue un fichier DFPWM local.")
+    print("  playURL <url>                     - Télécharge et joue un fichier DFPWM depuis une URL.")
+    print("  help                              - Affiche ce message d'aide.")
+end
+
+-- Commande principale
+local args = {...}
+
+if #args == 0 then
+    showHelp()
+    return
+end
+
+local command = args[1]
+
+if command == "playSound" then
+    local name = args[2]
+    local volume = tonumber(args[3]) or 1.0
+    local pitch = tonumber(args[4]) or 1.0
+
+    if not name then
+        error("Nom du son requis. Utilise : speaker playSound <nom> [volume] [pitch]")
     end
 
-    if pitch then
-        pitch = tonumber(pitch)
-        if not pitch then
-            error("Pitch must be a number", 0)
-        end
-        if pitch < 0 or pitch > 2 then
-            error("Pitch must be between 0 and 2", 0)
-        end
+    local speaker = peripheral.find("speaker")
+    if not speaker then
+        error("Aucun périphérique speaker détecté.")
     end
 
-    local speaker = get_speakers(name)[1]
+    speaker.playSound(name, volume, pitch)
+    print(string.format("Son joué : '%s' (Volume: %.2f, Pitch: %.2f)", name, volume, pitch))
 
-    if speaker.playSound(sound, volume, pitch) then
-        print(("Played sound %q on speaker %q with volume %s and pitch %s."):format(
-            sound, peripheral.getName(speaker), volume or 1, pitch or 1
-        ))
-    else
-        error(("Could not play sound %q"):format(sound), 0)
+elseif command == "playDFPWM" then
+    local filePath = args[2]
+    if not filePath then
+        error("Chemin du fichier requis. Utilise : speaker playDFPWM <fichier>")
     end
+    playDFPWM(filePath)
+
+elseif command == "playURL" then
+    local url = args[2]
+    if not url then
+        error("URL requise. Utilise : speaker playURL <url>")
+    end
+    playDFPWMFromURL(url)
+
+elseif command == "help" then
+    showHelp()
+
 else
-    local programName = arg[0] or fs.getName(shell.getRunningProgram())
-    print("Usage:")
-    print(programName .. " play <file or url> [speaker]")
-    print(programName .. " sound <sound> [volume] [pitch] [speaker]")
-    print(programName .. " stop [speaker]")
+    print("Commande inconnue :", command)
+    showHelp()
 end
